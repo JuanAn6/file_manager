4a6020b9b8b021b7c907c4943ee2d494f191fcea
JuanAn6 <juanantoniorizija666@gmail.com>
2025-11-05 23:32:56 +0100

    Implementation of JWT authentication

---
+++ api_file_manager/app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Exception;
+use Illuminate\Http\Request;
+
+class JwtMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
+
+// namespace App\Http\Middleware;
+
+// use Closure;
+// use Illuminate\Http\Request;
+// use Symfony\Component\HttpFoundation\Response;
+
+// class JwtMiddleware
+// {
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+//      */
+//     public function handle(Request $request, Closure $next): Response
+//     {
+//         return $next($request);
+//     }
+// }
