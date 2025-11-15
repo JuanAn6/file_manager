356b24dba2ff53d6687a7c76ec4cd3c82a704452
JuanAn6 <juanantoniorizija666@gmail.com>
2025-11-15 21:44:41 +0100

    Show hide vertical nav and styles imporvements

--- web_file_manager/src/App.jsx
+++ web_file_manager/src/App.jsx
@@ -4,12 +4,14 @@ import Login from './components/Login';
 import Home from './components/Home';
 import { useAuth } from './context/AuthContext';
 import api from './api/axios'; 
-import {  } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function App() {
   const navigate = useNavigate();
   const { logout, isAuthenticated } = useAuth();
+  
+  const [navExpanded, setNavExpanded] = useState(true);
 
   const handleLogout = async (evt) =>{
     evt.preventDefault();
@@ -22,21 +24,29 @@ function App() {
     }
   }
 
+  const handleExpandContractMenu = ()=>{
+    setNavExpanded(!navExpanded);
+    console.log(navExpanded);
+  }
+
   return (
     <div>
       {isAuthenticated && (
         <div>
-          <nav className='vertical-nav'>
+          <nav id="vertical-nav" className={'vertical-nav '+ (navExpanded? '':'position-out')}>
             <h2 className='no-margin'>File manager</h2>
+            <span>{navExpanded}</span>
           </nav>
           {/* Nav bar */}
-          <nav className='horizontal-nav vertical-expanded'>
+          <nav className={'horizontal-nav '+ (navExpanded? 'vertical-expanded ':'')}>
+            <button onClick={handleExpandContractMenu}>Show/hide</button>
             <Link to="/">Home</Link>
             <Link to="/" onClick={handleLogout}>Logout</Link>
           </nav>
         </div>
       )}
-      <main className='main-container vertical-expanded'>
+
+      <main className={isAuthenticated ? ('main-container '+ (navExpanded? 'vertical-expanded ':'')) : '' }>
         <Routes>
           {/* Public routes */}
           <Route path="login" element={<Login />} />
